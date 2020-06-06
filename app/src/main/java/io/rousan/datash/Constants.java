71bc0c305ee4cbff63567bf1c2f7664bfd2164ec
Rousan Ali <rousanali786@gmail.com>
2020-06-06 16:43:06 +0530

    Update log

--- app/src/main/java/io/rousan/datash/Constants.java
+++ app/src/main/java/io/rousan/datash/Constants.java
@@ -4,5 +4,5 @@ public class Constants {
     public final static String NOTIF_CHANNEL_DOWNLOAD_FILE = "DOWNLOAD_FILE";
 
     // public final static String DATASH_BASE_URL = "http://10.0.2.2:3000/";
-    public final static String DATASH_BASE_URL = "http://192.168.0.184:3000/";
+    public final static String DATASH_BASE_URL = "https://datash.co/";
 }
